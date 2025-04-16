739e596f86564fd1f78acdedd75bbab71556b52b
Madeleine Woodbury <mail@woodburycode.com>
2025-04-15 21:52:26 -0400

    Add new 3D models and update project settings for iOS support

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 
+/// <summary>
+/// This script rotates a GameObject around a specified axis at a specified speed.
+/// </summary>
 public class Rotator : MonoBehaviour
 {
     [SerializeField] float rotationSpeed = 2f;
     [SerializeField] Vector3 rotationAxis = Vector3.up;
 
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
     void Update()
     {
         transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);        
